40f0f2aba26e642aab361a95cdacba5486d383b7
Leo Honkanen <leo.o.honkanen@gmail.com>
2015-09-28 17:13:31 +0300

    deploy successful builds to heroku

--- src/main/java/com/mycompany/uutislukija/Main.java
+++ src/main/java/com/mycompany/uutislukija/Main.java
@@ -2,11 +2,24 @@
 package com.mycompany.uutislukija;
 
 import com.mycompany.hackernewsuutiset.HackerPaivanUutiset;
+import java.util.Map;
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args){
         HackerPaivanUutiset hakija = new HackerPaivanUutiset();
+
+	// try to configure a custom port
+        Map<String, String> env = System.getenv();
+	String portprop = env.get("PORT");
+        
+        if (portprop != null) {
+           try {
+                int portnum = Integer.parseInt(portprop);
+                port(portnum);
+            } catch (NumberFormatException e) {}
+        }   
+        
         get("/", (req, res) -> "<a href=/viimeisin>Viimeisin</a> <a href=/suosituin>Suosituin</a>");
         get("/viimeisin", (req, res) -> hakija.haeViimeisinUutinen());
         get("/suosituin", (req, res) -> hakija.haeSuosituinUutinen());

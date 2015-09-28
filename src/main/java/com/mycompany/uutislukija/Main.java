18656660683f2f2ed235db151b4e7ed02a8bbd0e
Leo Honkanen <leo.o.honkanen@gmail.com>
2015-09-28 18:36:34 +0300

    refactor and test a bit more

--- src/main/java/com/mycompany/uutislukija/Main.java
+++ src/main/java/com/mycompany/uutislukija/Main.java
@@ -2,26 +2,16 @@
 package com.mycompany.uutislukija;
 
 import com.mycompany.hackernewsuutiset.HackerPaivanUutiset;
+import com.mycompany.paivanuutiset.PaivanUutiset;
 import java.util.Map;
-import static spark.Spark.*;
 
 public class Main {
+    
     public static void main(String[] args){
-        HackerPaivanUutiset hakija = new HackerPaivanUutiset();
-
 	// try to configure a custom port
         Map<String, String> env = System.getenv();
 	String portprop = env.get("PORT");
-        
-        if (portprop != null) {
-           try {
-                int portnum = Integer.parseInt(portprop);
-                port(portnum);
-            } catch (NumberFormatException e) {}
-        }   
-        
-        get("/", (req, res) -> "<a href=/viimeisin>Viimeisin</a> <a href=/suosituin>Suosituin</a>");
-        get("/viimeisin", new ViimeisinRoute(hakija));
-        get("/suosituin", new SuosituinRoute(hakija));
+        PaivanUutiset hakija = new HackerPaivanUutiset();
+        new App(hakija, portprop).setUpAndRun();
     }
 }
